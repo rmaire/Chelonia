f6e0e26385c38195e5702505c2931999183d8884
Roman Maire <roman.maire@zuara.ch>
2022-06-01 22:00:01 +0200

    Cleanup

--- core/src/main/java/ch/uprisesoft/chelonia/repl/console/Log.java
+++ core/src/main/java/ch/uprisesoft/chelonia/repl/console/Log.java
@@ -9,7 +9,6 @@ import java.io.Writer;
 
 public class Log {
 	private Array<LogEntry> logEntries;
-	private int numEntries = Console.UNLIMITED_ENTRIES;
 
 	protected Log () {
 		logEntries = new Array<LogEntry>();
@@ -17,39 +16,9 @@ public class Log {
 
 	protected void addEntry (String msg, LogLevel level) {
 		logEntries.add(new LogEntry(msg, level));
-		if (logEntries.size > numEntries && numEntries != Console.UNLIMITED_ENTRIES) {
-			logEntries.removeIndex(0);
-		}
 	}
 
 	protected Array<LogEntry> getLogEntries () {
 		return logEntries;
-	}
-
-	public boolean printToFile (FileHandle fh) {
-		if (fh.isDirectory()) {
-			throw new IllegalArgumentException("File cannot be a directory!");
-		}
-
-		Writer out = null;
-		try {
-			out = fh.writer(false);
-		} catch (Exception e) {
-			return false;
-		}
-
-		String toWrite = "";
-		for (LogEntry l : logEntries) {
-			toWrite += l.toString() + "\n";
-		}
-
-		try {
-			out.write(toWrite);
-			out.close();
-			return true;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
 	}
 }
